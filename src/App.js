61ef97367ea629819dfc99e1acd25eb2552b0d09
yoosion030 <yoosioff@gmail.com>
2024-10-31 14:32:35 +0900

    feat: fractionNavigationOrder 테스트
    Signed-off-by: yoosion030 <yoosioff@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+
+import "//unpkg.com/mathlive";
 
 function App() {
+  useEffect(() => {
+    const mf = document.querySelector("math-field");
+    mf.setOptions({
+      fractionNavigationOrder: "denominator-numerator",
+    });
+
+    mf.value = "{1}\\frac{1}{2}";
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <math-field style={{ width: "100px" }}></math-field>
     </div>
   );
 }
